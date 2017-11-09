397a6da2393ba5612556901251627e9d4c424a22
Thomas Nairn <the_nairn@Thomass-MacBook-Pro.local>
2017-11-09 23:30:57 +0000

    Added standard linter

--- server/app/Controllers/Http/UserController.js
+++ server/app/Controllers/Http/UserController.js
@@ -1,43 +1,31 @@
 'use strict'
 
-const User = use('App/Models/User');
+const User = use('App/Models/User')
 
 class UserController {
-
-  async register({ request, auth }) {
-    const {
-      email,
-      password,
-      name
-    } = request.all();
+  async register ({ request, auth }) {
+    const { email, password, name } = request.all()
 
     const user = await User.create({
       name,
       email,
       password
-    });
+    })
 
-    const token = await auth.generate(user);
+    const token = await auth.generate(user)
 
-    return {
-      user,
-      token
-    };
+    return { user, token }
   }
 
-  async login({ request, auth }) {
-    const email = request.input('email');
-    const password = request.input('password');
-    try {
-      await auth.attempt(email, password);
-      const user = await User.query().where('email', email).first();
-      const token = await auth.generate(user);
-      response.json({
-        token,
-        user
-      });
-    } catch (e) {
-      response.unauthorized('Invalid credentails');
+  async login ({ request, response, auth }) {
+    const email = request.input('email')
+    const password = request.input('password')
+    await auth.attempt(email, password)
+    const user = await User.query().where('email', email).first()
+    const token = await auth.generate(user)
+    return {
+      token,
+      user
     }
   }
 }
